23092bb5cf1a3653e74f06d836d2653db6374862
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-24 15:44:37 +0000

    feat : add the middleware in the routes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,21 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/is-banned', function () {
+    return view('auth.is-banned');
+})->name('is-banned');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified','checkbanned'])->name('dashboard');
+
+Route::middleware(['auth','checkbanned'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
