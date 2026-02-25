65096a6567fcac1ba3babcdb9300af956d356613
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-25 22:02:42 +0000

    feat: create Colocation model with relationships to Membership, User, Invitation, Category, and Expense

---
+++ app/Models/Colocation.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Colocation extends Model
+{
+    protected $fillable = [
+        'name',
+        'status',
+        'description',
+    ];
+
+    public function memberships()
+    {
+        return $this->hasMany(Membership::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'memberships'
+        )->withPivot(['role', 'left_at'])
+            ->withTimestamps();
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(Invitation::class);
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+}
