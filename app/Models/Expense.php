e915f943919f31647a0cfc19fef561df80230ed5
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-25 22:02:55 +0000

    feat: create Expense model with relationships to Colocation, User, Category, and ExpenseDetail

---
+++ app/Models/Expense.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    protected $fillable = [
+        'name',
+        'colocation_id',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function splits()
+    {
+        return $this->hasMany(ExpenseDetail::class);
+    }
+    
+}
