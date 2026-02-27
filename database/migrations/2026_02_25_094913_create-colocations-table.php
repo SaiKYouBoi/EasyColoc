9829a086f008febd7a96a92f2caba2ff1855f0f2
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-27 06:09:32 +0000

    fix: rename 'name' attribute to 'title' in colocations migration

--- database/migrations/2026_02_25_094913_create-colocations-table.php
+++ database/migrations/2026_02_25_094913_create-colocations-table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('colocations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->enum('status',['active','cancelled'])->default('active');
             $table->text('description')->nullable();
             $table->timestamps();
